93df5afb4858c16ad39b394df1ffa2fc4ef3ca31
G8LOL <66517997+G8LOL@users.noreply.github.com>
2020-07-23 09:02:36 -0400

    Updated Module class

--- artemis/module.java
+++ artemis/module.java
@@ -2,6 +2,8 @@ package artemis;
 
 import java.util.Iterator;
 
+import artemis.events.Event;
+import artemis.logger.Logger;
 import artemis.notification.Notification;
 import artemis.notification.NotificationManager;
 import artemis.notification.NotificationType;
@@ -14,9 +16,12 @@ import net.minecraft.network.play.server.S02PacketChat;
 public class module {
 	
 	protected Minecraft mc = Minecraft.getMinecraft();
-	protected String name;
+	protected String name, hudname;
 	private String discription;
 	private int key, id;
+	
+	public int size = 0;
+	
 	public int getId() {
 		return id;
 	}
@@ -28,10 +33,11 @@ public class module {
 	private boolean toggled;
 	private Category category;
 	private boolean isEnabled;
-	public boolean visible = false;
+	public boolean visible = true;
 	
-	public module(String nm, String discription, int k, int id, Category c){
+	public module(String nm, String hudnm, String discription, int k, int id, Category c){
 		name = nm;
+		hudname = hudnm;
 		this.discription = discription;
 		key = k;
 		this.id = id;
@@ -43,52 +49,67 @@ public class module {
 	public String getDiscription() {
 		return discription;
 	}
+	public static module getModule(String name) {
+		for (module m : Artemis.getModules()) {
+			System.out.println("getModule works!!!");
+			if (m.getName() == name) {
+				System.out.println("getModule works even more!!!");
+				System.out.println(m);
+				return m;
+			}
+		}
+		return null;
+	}
 
 	public void setDiscription(String discription) {
 		this.discription = discription;
 	}
 
 	public void toggle(){
+		
 		toggled = !toggled;
 		if(toggled){
 			onEnable();
+			size = 0;
 		}else{
 			onDisable();
+			size = 0;
+		}
+		if (Artemis.instance.saveLoad != null) {
+			Artemis.instance.saveLoad.save();
 		}
 	}
 	
 	public void onEnable(){
-		NotificationManager.show(new Notification(NotificationType.INFO, "Info", name + " was enabled", 2));
-		 for(Iterator<Object> entities = mc.theWorld.loadedEntityList.iterator(); entities.hasNext();) {
-	          Object theObject = entities.next();
-	          if(theObject instanceof EntityLivingBase) {
-	              EntityLivingBase entity = (EntityLivingBase) theObject;
-	             
-	              if(entity instanceof EntityPlayerSP) continue;
-	             
-	              if(mc.thePlayer.getDistanceToEntity(entity) <= 6.2173613F) {
-	                  if(entity.isEntityAlive()) {
-	                	  NotificationManager.show(new Notification(NotificationType.WARNING, "WARNING!", " Entity near you!", 2));
-	                  }
-                   }
-	          }
-		 }
-		 for(Object entity : mc.theWorld.loadedEntityList)
-             if(((Entity)entity).isInvisible() && entity != mc.thePlayer) {
-            	 NotificationManager.show(new Notification(NotificationType.ERROR, "WARNING!", "Watchdog detected!", 2));
-             }
+		 Logger.instance.log(hudname + " was enabled", Logger.LogType.NORMAL);
+		NotificationManager.show(new Notification(NotificationType.ENABLE, hudname, hudname + " was enabled", 2));
 	}
 	public void onDisable(){
-		NotificationManager.show(new Notification(NotificationType.INFO, "Info", name + " was disabled", 2));
+		 Logger.instance.log(hudname + " was disabled", Logger.LogType.NORMAL);
+		NotificationManager.show(new Notification(NotificationType.DISABLE, hudname, hudname + " was disabled", 2));
 	}
 	public void onUpdate(){}
+	public void onAttack(){}
 	public void onRender(){}
+	public void onEvent(Event e){
+		
+	}
+
 
 	public String getName() {
-		return name;
+		return this.name;
+	}
+
+	public String getHudname() {
+		return hudname;
+	}
+
+	public void setHudname(String hudname) {
+		this.hudname = hudname;
 	}
 
 	public void setName(String name) {
+		
 		this.name = name;
 	}
 	
@@ -102,26 +123,72 @@ public class module {
 
 	public void setKey(int key) {
 		this.key = key;
+		if (Artemis.instance.saveLoad != null) {
+			Artemis.instance.saveLoad.save();
+		}
 	}
 
 	public boolean isToggled() {
 		return toggled;
 	}
 
-	public void setToggled(boolean toggled) {
+	
+	/*
+	 * 
+    public void setToggled(boolean toggledTo) {
+        if(toggledTo) {
+            if(getState()) {
+                return;
+            }
+            toggle();
+            return;
+        } else {
+            if(getState()) {
+                toggle();
+            }
+            return;
+        }
+    }
+    public void setToggled(boolean toggled) {
 		this.toggled = toggled;
+		
+		if (this.toggled) {
+			this.onEnable();
+		} else {
+			this.onDisable();
+		}
+		if (Artemis.instance.saveLoad != null) {
+			Artemis.instance.saveLoad.save();
+		}
 	}
+	 */
+	
+	public void setToggled(boolean toggledTo) {
+        if(toggledTo) {
+            if(isToggled()) {
+                return;
+            }
+            toggle();
+            return;
+        } else {
+            if(isToggled()) {
+                toggle();
+            }
+            return;
+        }
+    }
+
 	public final boolean isCategory(Category s) {
 		if (s == category)
 			return true;
 		return false;
 	}
 	public boolean getState() {
-		return isEnabled;
-	}
+		return isToggled();
+	}\
 	
 	public void setState(boolean state) {
-		this.toggle();
+		this.onEnable();
 		if (state) {
 			this.onEnable();
 			this.isEnabled = true;
@@ -142,6 +209,16 @@ public class module {
 	public void setup() {
 		
 		
+	}
+
+	public String getMode() {
+		// TODO Auto-generated method stub
+		return "Vanilla";
+	}
+
+	public String getOther() {
+		// TODO Auto-generated method stub
+		return "N/A";
 	}
 	
 	
